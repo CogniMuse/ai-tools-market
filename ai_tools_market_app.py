4260a6323b13e9756f14426216db57cd33759411
Prashanth <prash4nth@gmail.com>
2023-09-19 02:35:09 +0530

    Feat: Added Tool Percentages

--- ai_tools_market_app.py
+++ ai_tools_market_app.py
@@ -3,36 +3,52 @@ import streamlit as st
 
 # Data loading and preprocessing
 
+#TODO preprocess and save file to improve performance
 df = pd.read_csv("Futurepedia_Ai_Tools_Dataset.csv",encoding='cp1252')
 df['Categories'] = df['Categories'].fillna('')
 df['Categories'] = df['Categories'].map(lambda x: x.split('\n'))
 df['Site url'] = df['Site url'].map(lambda x: x.split('?')[0])
 df['Categories'] = df['Categories'].fillna('')
 
+
 # Exploding data
 exploded = df.explode('Categories')
+
+#Constants
+tools_total_count = exploded.shape[0]
+categories_total = len(exploded['Categories'].unique())
+subheading = '<p style="color:#ccff00">Hello!</p>'
 
 
 st.set_page_config(layout='wide')
 
-st.write("# AI Tools Market 💹")
-st.write("Understand the AI tools market")
+st.title(body="AI Tools Market 💹 by [Cognimuse.com](https://www.cognimuse.com/)")
+st.write("### Explore a wide range of AI tools and understand the AI tools market")
+st.write("""** ** """)
+# st.markdown(subheading,unsafe_allow_html=True)
 
 if 'category' not in st.session_state:
-    st.session_state.category = '#fitness'
+    st.session_state.category = '#fun tools'
+
+if 'count' not in st.session_state:
+    st.session_state.count = exploded[exploded['Categories']=='#fun tools'].shape[0]
 
 
 def click(label):
     st.session_state.category = label
-    
-with st.sidebar:
-    btn = [st.button(label=x,on_click=click,args=[x]) for x in exploded['Categories'].value_counts().keys().values]
+    st.session_state.count = exploded[exploded['Categories']==st.session_state.category].shape[0]
 
+#Sidebar 
+st.sidebar.title("Categories",)
+st.sidebar.header("Total Categories: {0}".format(categories_total))
+btn = [st.sidebar.button(label=x,on_click=click,args=[x]) for x in exploded['Categories'].value_counts().keys().values]
 
-st.dataframe(exploded[exploded['Categories']==st.session_state.category],
+st.write("#### Number of tools in ",":green["+st.session_state.category+']'," : ",
+         st.session_state.count)
+
+st.write("#### Percentage of  :green["+st.session_state.category+"] tools: ",round(st.session_state.count/tools_total_count*100,2),':green[%]' )
+
+st.dataframe(exploded[exploded['Categories']==st.session_state.category][['Ai Tools Name','Types For Use','Pricing','Site url']],
              use_container_width=True,
              column_config= {"Site url":st.column_config.LinkColumn()},
              hide_index=True)
-
-st.bar_chart(x=list(exploded['Categories'].value_counts().keys()),y=list(exploded['Categories'].value_counts()))
-
