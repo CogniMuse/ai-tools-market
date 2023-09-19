87c4a0023e5a06edea9fd6cba63a00a1f6ec5445
Prashanth <prash4nth@gmail.com>
2023-09-19 14:23:32 +0530

    Feat: Bar chart distribution of tools

--- ai_tools_market_app.py
+++ ai_tools_market_app.py
@@ -15,15 +15,14 @@ df['Categories'] = df['Categories'].fillna('')
 exploded = df.explode('Categories')
 
 #Constants
-tools_total_count = exploded.shape[0]
+tools_total_count = df.shape[0]
 categories_total = len(exploded['Categories'].unique())
 subheading = '<p style="color:#ccff00">Hello!</p>'
-
 
 st.set_page_config(layout='wide')
 
 st.title(body="AI Tools Market 💹 by [Cognimuse.com](https://www.cognimuse.com/)")
-st.write("### Explore a wide range of AI tools and understand the AI tools market")
+st.write("### Explore over 3000 AI tools and understand the AI tools market")
 st.write("""** ** """)
 # st.markdown(subheading,unsafe_allow_html=True)
 
@@ -43,12 +42,22 @@ st.sidebar.title("Categories",)
 st.sidebar.header("Total Categories: {0}".format(categories_total))
 btn = [st.sidebar.button(label=x,on_click=click,args=[x]) for x in exploded['Categories'].value_counts().keys().values]
 
+# Main area
 st.write("#### Number of tools in ",":green["+st.session_state.category+']'," : ",
          st.session_state.count)
 
-st.write("#### Percentage of  :green["+st.session_state.category+"] tools: ",round(st.session_state.count/tools_total_count*100,2),':green[%]' )
+st.write("#### Percentage of  :green["+st.session_state.category+"] tools: ",
+         round(st.session_state.count/tools_total_count*100,2),':green[%]' )
 
 st.dataframe(exploded[exploded['Categories']==st.session_state.category][['Ai Tools Name','Types For Use','Pricing','Site url']],
              use_container_width=True,
              column_config= {"Site url":st.column_config.LinkColumn()},
              hide_index=True)
+
+# Bar chart distribution of AI tools
+st.write("""** ** """)
+
+st.write('### Categorical distribution of AI tools')
+st.bar_chart(exploded['Categories'].value_counts(),color="#ccff00",)
+
+#st.bar_chart(exploded[exploded['Categories']=='#fun tools']['Pricing'].value_counts(),color="#ccff00")
